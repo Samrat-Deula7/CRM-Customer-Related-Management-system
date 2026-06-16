10af16f61ba7ce099c15f9deed70c9d4c1194d0c
Samrat-Deula7 <samratdeula461@gmail.com>
2026-06-16 15:52:50 +0545

    Working on setting the backend controller, services and module for visa_ranking

--- backend/src/visa_ranking/visa_ranking.controller.ts
+++ backend/src/visa_ranking/visa_ranking.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import {VisaRankingService} from "@visa_ranking/visa_ranking.service"
 
 @Controller('visa-ranking')
-export class VisaRankingController {}
+export class VisaRankingController {
+    constructor(
+        private visaRanking: VisaRankingService
+    ){}
+}
